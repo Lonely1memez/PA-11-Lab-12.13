43a576fd806a1bf83d2208a6df9cf6ab3dcfa6e2
Lonely.memez <92387289+Lonely1memez@users.noreply.github.com>
2022-11-14 21:06:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@PA-11-Lab-1213/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int DigitCount(int number) 
+{
+     if (number/10 == 0)
+      return 1;
+      return 1 + DigitCount(number / 10);
+}
+
+
+int main() 
+{
+  int num;
+  int digits;
+
+  cin >> num;
+  digits = DigitCount(num);
+  cout << digits << endl;
+  return 0;
+}
